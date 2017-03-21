f008d67b413730718c4a26504aa0c80e4440dad1
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-21 14:28:26 +0000

    debug top up test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,14 +5,12 @@ describe Oystercard do
   subject(:oystercard) {described_class.new}
 
   describe '#balance' do
-
     it "shows balance on the card" do
       expect(oystercard.balance).to eq 0
     end
   end
 
   describe '#top_up' do
-
     it "adds money to the card" do
       expect{ oystercard.top_up(20) }.to change{ subject.balance }.by 20
     end
@@ -22,6 +20,7 @@ describe Oystercard do
       # subject.top_up(maximum_balance)
       expect {oystercard.top_up(maximum_balance+1)}.to raise_error "Maximum balance of #{maximum_balance} reached"
     end
+  end
 
   describe '#deduct' do
     it 'deducts fare from balance' do
@@ -29,6 +28,4 @@ describe Oystercard do
       expect { oystercard.deduct 5}.to change{ oystercard.balance }.by -5
     end
   end
-end
-
 end
