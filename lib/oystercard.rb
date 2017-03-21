022743fd1eea261f4683dc6219563e9dd8c18d8f
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-21 14:12:02 +0000

    implement fail guard to raise error if topup exceeds maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,18 @@
 class Oystercard
 
+  MAXIMUM_BALANCE = 90
+
+
   attr_reader :balance
-  attr_writer :balance
+
 
   def initialize
     @balance = 0
   end
 
   def top_up(money)
-    self.balance += money
+    fail "Maximum balance of #{MAXIMUM_BALANCE} reached" if @balance + money > MAXIMUM_BALANCE
+    @balance += money
   end
 
 end
