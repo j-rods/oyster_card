2156ca9ec45cd9c13076712e460b451fdd104b12
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-23 08:27:57 +0000

    create test for station to know name and zone

---
+++ spec/station_spec.rb
@@ -0,0 +1,14 @@
+require 'station'
+
+describe Station do
+  subject(:station) {described_class.new(name: "Aldgate", zone: 1)}
+
+  it "knows its name" do
+    expect(station.name).to eq("Aldgate")
+  end
+
+  it "knows its zone" do
+    expect(station.zone).to eq(1)
+  end
+
+end
