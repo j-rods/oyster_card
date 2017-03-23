8f6b51a5dba67e64406ce900301bfb8c3e96104a
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-23 16:20:04 +0000

    refactor obsolete code, implement applying correct fare functionality from journey object

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,17 +5,19 @@ class Oystercard
 
   MAXIMUM_BALANCE = 90
   MINIMUM_TRAVEL_BALANCE = 1
+  PENALTY_CHARGE = 6
 
 
-  attr_reader :balance, :fare, :entry_station, :exit_station, :in_use, :journey_history
+  attr_reader :balance, :fare, :entry_station, :exit_station, :in_use, :journey_history, :journey
 
 
-  def initialize(fare=MINIMUM_TRAVEL_BALANCE)
+  def initialize
     @balance = 0
-    @fare = fare
+    @fare = MINIMUM_TRAVEL_BALANCE
     @entry_station = nil
     @exit_station = nil
     @in_use = false
+    # @track_journey = nil
     @journey_history = []
   end
 
@@ -26,28 +28,32 @@ class Oystercard
 
   def touch_in(station)
     fail "Insufficient funds. £1 minimum needed to travel." if @balance < MINIMUM_TRAVEL_BALANCE
-    self.in_use = true
-    @entry_station = station
-  end
+    # is track true?
+    #   apply penalty fare and continue journey as usual
 
-  def in_journey?
-    self.in_use
+    # @track_journey = true
+    if defined?(journey.fare)
+      deduct(journey.fare)
+    else
+      @journey = Journey.new(station)
+    end
+    self.journey.start_journey
   end
 
   def touch_out(station)
-    deduct(fare)
-    self.in_use = false
-    self.exit_station = station
-    self.add_to_journey_history
+    self.journey.end_journey(station)
+    deduct(journey.fare)
+    self.add_to_journey_history(station)
+    # @track_journey = false / add to journey - apply fare
   end
 
-  def add_to_journey_history
-    self.journey_history << {entry_station: self.entry_station, exit_station: self.exit_station}
+  def add_to_journey_history(station)
+    self.journey_history << {entry_station: journey.entry_station, exit_station: station}
   end
 
   private
 
-  attr_writer :deduct, :balance, :exit_station, :in_use
+  attr_writer :deduct, :balance, :exit_station, :in_use, :journey
 
   def deduct(fare)
     @balance -= fare
