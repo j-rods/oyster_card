80d5d821022771a36a4792c040492c0c6eb41bc4
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-23 16:18:40 +0000

    implement functionality to calculate correct fare for journeys

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,9 +2,39 @@ class Journey
 
   attr_reader :entry_station, :ended
 
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
+
   def initialize(station)
     @entry_station = station
-    @ended = false
+    @fare = nil
+    @ended = nil
   end
+
+  def start_journey
+    self.ended = false
+  end
+
+  def end_journey(station)
+    self.ended = true
+    self
+  end
+
+  def fare
+    if ended == true
+      self.fare = MINIMUM_FARE
+    elsif ended == false
+      self.fare = PENALTY_FARE
+    else fare = 0
+    end
+  end
+
+  def complete?
+  ended
+  end
+
+  private
+
+  attr_writer :ended, :fare
 
 end
