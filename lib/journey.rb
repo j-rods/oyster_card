ca08710b6e82811fa0f7328681b512bb107a149e
Freddy Fallon <freddyfallon@gmail.com>
2017-03-23 11:27:25 +0000

    Add ended instance variable and test to change status

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,10 +1,10 @@
 class Journey
 
-  attr_reader :entry_station
+  attr_reader :entry_station, :ended
 
   def initialize(station)
     @entry_station = station
+    @ended = false
   end
 
-  
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,13 +2,25 @@ require 'journey'
 
 describe Journey do
   let(:station) {double(:station)}
-  subject {described_class.new(station)}
+  subject(:journey) {described_class.new(station)}
 
   describe '#entry_station' do
     it {is_expected.to respond_to :entry_station}
     it "is expected to contain station" do
-      expect(subject.entry_station).to eq station
+      expect(journey.entry_station).to eq station
     end
   end
 
+  describe '#ended' do
+    it "is not ended" do
+      expect(journey.ended).to be false
+    end
+  end
+
+  describe '#end_journey' do
+    it 'ends the journey' do
+      journey.end_journey
+      expect(journey.ended).to be true
+    end
+  end
 end
