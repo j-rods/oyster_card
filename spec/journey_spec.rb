b15a001a8b7c855fe63b25e4a414a19767a1d48d
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-23 16:18:00 +0000

    create tests to check for incomplete journeys and apply correct fare

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -5,22 +5,74 @@ describe Journey do
   subject(:journey) {described_class.new(station)}
 
   describe '#entry_station' do
-    it {is_expected.to respond_to :entry_station}
-    it "is expected to contain station" do
-      expect(journey.entry_station).to eq station
-    end
-  end
+    context 'when starting a journey' do
+      it "it's expected to set an entry station" do
+        expect(journey.entry_station).to eq station
+      end
 
-  describe '#ended' do
-    it "is not ended" do
-      expect(journey.ended).to be false
+      describe '#start_journey' do
+        it 'tracks a new journey' do
+          journey.start_journey
+          expect(journey.ended).to be false
+        end
+      end
     end
   end
 
   describe '#end_journey' do
     it 'ends the journey' do
-      journey.end_journey
+      journey.end_journey(station)
       expect(journey.ended).to be true
     end
+
+    it 'returns itself when exiting journey' do
+      expect(journey.end_journey(station)).to eq(journey)
+    end
   end
+
+  describe '#fare' do
+    it 'has a penalty fare by default' do
+      journey.start_journey
+      expect(journey.fare).to eq Journey::PENALTY_FARE
+    end
+
+
+    it 'is initially equal to the minimum fare' do
+    allow(journey).to receive(:ended).and_return(true)
+    expect(journey.fare).to eq 1
+    end
+
+    context 'given an entry station' do
+      subject {described_class.new(entry_station: station)}
+      it "returns a penalty fare if no exit station given" do
+        journey.start_journey
+        expect(journey.fare).to eq Journey::PENALTY_FARE
+      end
+    end
+
+    context 'given an exit station' do
+      let(:other_station) { double :other_station }
+
+      before do
+        journey.end_journey(other_station)
+      end
+
+      it 'calculates a fare' do
+        expect(journey.fare).to eq 1
+      end
+
+      it 'knows if a journey is complete' do
+        expect(journey).to be_complete
+      end
+
+    end
+
+  end
+
+  describe '#complete?' do
+    it 'knows if a journey is not complete' do
+      expect(journey).not_to be_complete
+    end
+  end
+
 end
