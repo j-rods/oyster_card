fd58ef947e2eab5a4a67e0878f83683c160d48c0
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-24 10:17:31 +0000

    create test for starting journeys and current journeys

--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -1,7 +1,23 @@
 require 'journeylog'
 
 describe JourneyLog do
+  subject(:journeylog) {described_class.new}
+  let(:entry_station) {:entry_station}
+
   describe '#journey_log' do
     it {is_expected.to respond_to(:journey_log)}
   end
+
+  describe '#start' do
+  	it'starts a journey' do
+  	  expect(journeylog.start(entry_station)).to eq entry_station 
+    end
+  end
+
+  describe '#current_journey' do
+  	it'returns incomplete journey or creates new journey' do
+  	  expect(journeylog.current_journey).to eq nil	
+  	end	
+  end
+
 end
