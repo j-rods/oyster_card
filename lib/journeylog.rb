24ac7d432c9b98faa2aed2ce8e66d68a0ac0679e
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-24 10:35:57 +0000

    implement finish journey functionality

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,18 +1,24 @@
 class JourneyLog
 
-  attr_reader :journey_log
+  attr_reader :journey_log, :journey_class
 
-  def initialize
+  def initialize(journey_class)
     @journey_log
+    @journey_class
   end
 
   def start(entry_station)
-  	entry_station
+  	@journey_class = entry_station
+  end
+
+  def finish
+
   end
 
   #private
 
   def current_journey
+
   end
 
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -1,23 +1,33 @@
 require 'journeylog'
 
 describe JourneyLog do
-  subject(:journeylog) {described_class.new}
+  subject(:journeylog) {described_class.new(journey)}
   let(:entry_station) {:entry_station}
 
-  describe '#journey_log' do
-    it {is_expected.to respond_to(:journey_log)}
-  end
+  let(:journey) { double(:journey_class) }
+
+  # describe 'Class initialization' do
+  #   it 'Creates journey log class' do
+  #     expect(journey_log).to
+  #   end
+  # end
 
   describe '#start' do
   	it'starts a journey' do
-  	  expect(journeylog.start(entry_station)).to eq entry_station 
+  	  expect(journeylog.start(entry_station)).to eq entry_station
     end
   end
 
   describe '#current_journey' do
   	it'returns incomplete journey or creates new journey' do
-  	  expect(journeylog.current_journey).to eq nil	
-  	end	
+  	  expect(journeylog.current_journey).to eq nil
+  	end
+  end
+
+  describe '#finish' do
+    it 'adds an exit station to current journey' do
+      expect(journeylog.finish).to eq nil
+    end
   end
 
 end
