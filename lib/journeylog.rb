2b5a8650b51e1e45ca36f5c4804a77574ac701a7
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-24 11:01:56 +0000

    Refactor functionality for creating new journeys and incomplete journeys

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,14 +1,15 @@
 class JourneyLog
 
-  attr_reader :journey_log, :journey_class
+  attr_reader :journey_log, :journey_class, :entry_station
 
   def initialize(journey_class)
     @journey_log
     @journey_class
+    @entry_station = nil
   end
 
   def start(entry_station)
-  	@journey_class = entry_station
+  	@entry_station = entry_station
   end
 
   def finish
@@ -18,7 +19,12 @@ class JourneyLog
   #private
 
   def current_journey
-
+    if !!entry_station
+      journey_class = Journey.new(entry_station) 
+      journey_class.entry_station
+    else
+      "Incomplete journey"
+    end
   end
 
 end
