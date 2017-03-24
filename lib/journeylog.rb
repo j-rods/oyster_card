184b682b7e9905d4e1f7987dc4badda87c537ee3
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-24 11:24:41 +0000

    implement journeys journey functionality

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,30 +1,36 @@
 class JourneyLog
 
-  attr_reader :journey_log, :journey_class, :entry_station
+  attr_reader :journey_log, :journey_class, :entry_station, :exit_station
 
   def initialize(journey_class)
-    @journey_log
+    @journey_log = []
     @journey_class
     @entry_station = nil
+    @exit_station = nil
   end
 
   def start(entry_station)
   	@entry_station = entry_station
   end
 
-  def finish
-
+  def finish(exit_station)
+    @journey_class.end_journey(exit_station)
+    exit_station = exit_station
   end
 
-  #private
+  private
 
   def current_journey
     if !!entry_station
-      journey_class = Journey.new(entry_station) 
-      journey_class.entry_station
+      @journey_class = Journey.new(entry_station)
+      @journey_class.entry_station
     else
       "Incomplete journey"
     end
+  end
+
+  def journeys
+    @journey_log << {entry_station: entry_station, exit_station: exit_station}
   end
 
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -3,6 +3,7 @@ require 'journeylog'
 describe JourneyLog do
   subject(:journeylog) {described_class.new(journey)}
   let(:entry_station) {:entry_station}
+  let(:exit_station) {:exit_station}
 
   let(:journey) { double(:journey_class) }
 
@@ -12,22 +13,31 @@ describe JourneyLog do
     end
   end
 
-  describe '#current_journey' do
-  	it'returns incomplete journey' do 
-      expect(journeylog.current_journey).to eq "Incomplete journey"
-  	end
-    
-    it 'creates new journey' do
-      journeylog.start(entry_station)	
-  	  expect(journeylog.current_journey).to eq entry_station
-  	end
+  # describe '#current_journey' do
+  # 	it'returns incomplete journey' do
+  #     expect(journeylog.current_journey).to eq "Incomplete journey"
+  # 	end
+  #
+  #   it 'creates new journey' do
+  #     journeylog.start(entry_station)
+  # 	  expect(journeylog.current_journey).to eq entry_station
+  # 	end
+  #
+  # end
+  #
+  # describe '#finish' do
+  #   it 'adds an exit station to current journey' do
+  #     journeylog.start(entry_station)
+  #     journeylog.current_journey
+  #     expect(journeylog.finish(exit_station)).to eq exit_station
+  #   end
+  # end
+  # 
+  # describe '#journeys' do
+  #   it 'Returns journey log' do
+  #     expect(journeylog.journeys).to eq journeylog.journey_log
+  #   end
+  # end
 
-  end
-
-  describe '#finish' do
-    it 'adds an exit station to current journey' do
-      expect(journeylog.finish).to eq nil
-    end
-  end
 
 end
